fdde98d2cc80633ae077e4eabf78a88664a8caf7
谢伟 <xiewei@xieweideMacBook-Air.local>
2018-07-07 20:54:43 +0800

    :tada:

--- ui/api-server/api_server.go
+++ ui/api-server/api_server.go
@@ -1,70 +1,83 @@
 package api_server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+)
 
 type APIServer struct {
 	engine *gin.Engine
 }
 
 func (a *APIServer) registry() {
-
-	index := a.engine.Group("")
-	{
-		index.GET("", HelloWorld)
-	}
-
-	matches := a.engine.Group("/api/matches")
-	{
-		matches.GET("/group", GroupPhase)
-		matches.GET("/knockout", KnockOutPhase)
-
-	}
-
-	teams := a.engine.Group("/api/teams")
-	{
-		teams.GET("/lists", TeamList)
-
-	}
-
-	groups := a.engine.Group("/api/groups")
-	{
-		groups.GET("/lists", GroupsList)
-
-	}
-
-	player := a.engine.Group("/api/players")
-	{
-		player.GET("/lists")
-	}
-
-	coaches := a.engine.Group("api/coaches")
-	{
-		coaches.GET("/lists", CoachesList)
-
-	}
-
-	statistics := a.engine.Group("/api/statistics")
-	{
-		statistics.GET("/players/goals", StatisticsPlayerGoal)
-		statistics.GET("/players/saves", StatisticsPlayerTop)
-		statistics.GET("/players/shots", StatisticsPlayerShots)
-		statistics.GET("/players/disciplinary", StatisticsPlayerDisciplinary)
-	}
-
-	awards := a.engine.Group("/api/awards")
-	{
-		awards.GET("/lists", AwardsList)
-	}
-
-	classic := a.engine.Group("/api/classic")
-	{
-		classic.GET("/lists", ClassicLists)
-		classic.GET("/lists/:year", ClassicListbyYear)
-	}
+	APIServerInit(a.engine)
 
 }
 
 func (a *APIServer) init() {
+
+}
+
+func APIServerInit(r *gin.Engine) {
+	// docs
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	v1 := r.Group("/api")
+
+	indexRegistry(v1)
+	matchesRegistry(v1)
+	teamsRegistry(v1)
+	groupsRegistry(v1)
+	playerRegistry(v1)
+	coachesRegistry(v1)
+	statisticsRegistry(v1)
+	awardsRegistry(v1)
+	classicRegistry(v1)
+
+}
+
+func indexRegistry(index *gin.RouterGroup) {
+	index.GET("", HelloWorld)
+
+}
+
+func matchesRegistry(matches *gin.RouterGroup) {
+	matches.GET("/matches/group", GroupPhase)
+	matches.GET("/matches/knockout", KnockOutPhase)
+
+}
+
+func teamsRegistry(teams *gin.RouterGroup) {
+	teams.GET("/teams/lists", TeamList)
+}
+
+func groupsRegistry(groups *gin.RouterGroup) {
+	groups.GET("/groups/lists", GroupsList)
+
+}
+
+func playerRegistry(player *gin.RouterGroup) {
+	player.GET("/players/lists")
+}
+
+func coachesRegistry(coaches *gin.RouterGroup) {
+	coaches.GET("/coaches/lists", CoachesList)
+}
+
+func statisticsRegistry(statistics *gin.RouterGroup) {
+	statistics.GET("/statistics/players/goals", StatisticsPlayerGoal)
+	statistics.GET("/statistics/players/saves", StatisticsPlayerTop)
+	statistics.GET("/statistics/players/shots", StatisticsPlayerShots)
+	statistics.GET("/statistics/players/disciplinary", StatisticsPlayerDisciplinary)
+}
+
+func awardsRegistry(awards *gin.RouterGroup) {
+	awards.GET("/awards/lists", AwardsList)
+}
+
+func classicRegistry(classic *gin.RouterGroup) {
+	classic.GET("/classic/lists/:year", ClassicListbyYear)
+	classic.GET("/classic/lists", ClassicLists)
 
 }
 
